24c0d4b0ca2fb84efd580ebf24a96b339582fb9d
Pravi Jain <parasjain.pj902@gmail.com>
2025-04-22 18:28:34 +0530

    fix the env

--- backend/back/src/main.ts
+++ backend/back/src/main.ts
@@ -6,16 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT')|| 3000;
-
+  const PORT = process.env.PORT || configService.get<number>('PORT') || 3000;
 
   app.enableCors({
     origin: 'http://localhost:3001',
-    methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials:true,
-  })
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-  await app.listen(PORT,'0.0.0.0');
-  console.log(`Backend is running on ${PORT}`);
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`✅ Backend is running on port ${PORT}`);
 }
 bootstrap();
